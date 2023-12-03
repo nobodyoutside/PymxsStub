274eddc29eb023df26bc15fbc71e4cbbe6c16967
살균표백 <Nobodyoutside@outlook.com>
2023-12-03 23:52:00 +0900

    설치용 파일 추가

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PymaxStub',
+    version='0.0.1',
+    description='3ds Max Python Stub',
+    url='https://github.com/nobodyoutside/PymxsStub.git',
+    author='nobodyoutside',
+    author_email="nobodyoutside@outlook.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
